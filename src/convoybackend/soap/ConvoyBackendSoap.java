983c1d76ff8dd8be67d89daf7fe47dbd171498b5
ministeren <fcmikkelsen@gmail.com>
2016-03-15 10:52:52 +0100

    navne...

--- src/convoybackend/soap/ConvoyBackendSoap.java
+++ src/convoybackend/soap/ConvoyBackendSoap.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package convoybackendrmi;
+package convoybackend.soap;
 
 /**
  *
  * @author ministeren
  */
-public class ConvoyBackendRmi {
+public class ConvoyBackendSoap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
