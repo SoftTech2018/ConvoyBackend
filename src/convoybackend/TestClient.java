697f5a7fc5e12813bd90374c3f056e87a3dd3e79
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2016-04-09 18:14:57 +0200

    Tilføjelse: Testmetode til POST i TestClient. Det er nemmere at teste herfra, end fra appen.

--- src/convoybackend/TestClient.java
+++ src/convoybackend/TestClient.java
@@ -6,7 +6,18 @@
 package convoybackend;
 
 import convoybackend.soap.SoapInt;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
@@ -26,5 +37,74 @@ public class TestClient {
         
         System.out.println(soapserv.Hent("test"));
         
+        /*
+        Jons testkode
+        */
+//        TestClient testClient = new TestClient();
+//        HashMap<String, String> params = new HashMap<>();
+//        for(int i=0; i<10; i++){
+//            params.put("testKey" + i, "testValue" + i);
+//        }
+//        String url = "http://localhost:8080";
+//        String response = testClient.performPostCall(url, params);
+//        System.out.println(response);
+    }
+    
+    /**
+     * Test af POST metode til REST server
+     * @param requestURL Url til REST server
+     * @param postDataParams
+     * @return 
+     */
+     private String performPostCall(String requestURL, HashMap<String, String> postDataParams) {
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+//            conn.setDoInput(true);
+//            conn.setDoOutput(true);
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response+=line;
+                }
+            }
+            else {
+                response="";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first){
+                first = false;
+            } else {
+                result.append("&");
+            }
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
     }
 }
